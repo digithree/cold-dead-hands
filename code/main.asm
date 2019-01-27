ff8ee0d299ae821ce8b0d94abc97e9199237b76c
digithree <1c3k1ng@tutanota.com>
2019-01-27 14:55:11 +0000

    Added player sprite and hooked up movement

--- code/main.asm
+++ code/main.asm
@@ -12,6 +12,7 @@
 STD_INT         = $EA31     ; KERNAL standard interrupt service routine to handle keyboard scan, cursor display etc.
 FN_SCR_WRITE_F  = $BDCD     ; Write integer value in A/X onto screen, in floating-point format.
 FN_CHROUT       = $FFD2     ; Write byte to default output, input A = byte to write
+FN_GETIN        = $FFE4
 
 ; DATA STARTS HERE
 
@@ -39,11 +40,17 @@ FN_CHROUT       = $FFD2     ; Write byte to default output, input A = byte to wr
 entry_and_first_time_setup
   lda #$24        ; set sprite data of player character (at block 36/0x24) = $0900 for sprite 0
   sta $07F8
+  lda #$0A        ; set main color to 1 - WHITE for sprite 0
+  sta $D027
+  lda #$09        ; set multi-color 1 to 9 - BROWN
+  sta $D025
+  lda #$02        ; set multi-color 2 to 2 - RED
+  sta $D026
   lda #$01        ; turn on bit 0 of multi-color mode indicator, sprite 0 to multicolor mode only
   sta $D01C
   lda #$03
   sta $D01D       ; bit 0 and 1 on for horiztonal expand for sprite 0 and 1
-  sta $D017       ; bit 0 and 1 on for horiztonal expand for sprite 0 and 1
+  sta $D017       ; bit 0 and 1 on for vertical expand for sprite 0 and 1
   ;jsr clear_screen
   jsr black_screen
 ;draw_character
@@ -63,51 +70,70 @@ map_screen_loop
   clc           ; clear carry flag before adding (would influence result if set)
   adc const_map_img_base_low  ; add map number index to base img high byte
   tax             ; A -> X, X is param for draw_screen base page
-  ;jsr draw_screen
+  jsr draw_screen
 setup_sprite
   ; TODO
 position_loop
   ; get current position info for map
-  lda #<map_meta_data_table     ; move map meta data table base adddres to zero page
-  sta $14
-  lda #>map_meta_data_table
-  sta $15
-  lda var_map_cur               ; use meta data table to get address position list for map number var_map_cur
-  lsr                           ; need to first multiply by 2 (2 bytes per address)
-  tay                           ; then store in Y for indirect look up offset
-  lda ($14), Y
-  sta $16                       ; store low / high byte in $16 / $17, for further look up
-  iny
-  lda ($14), Y
+  lda #$60          ; store high byte of base positions for map infos ($60XX) in $16 / $17, for further look up
   sta $17
-  ldy #$00                      ; reset Y counter for position 0, to which we'll add var_map_pos
-  ;lda ($16), Y    ; number of positions
-  ;tax
-  ldx var_map_pos               ; set X = loop counter, +1 as we'll skip to infos for that position by looping via X
+  ; move pointer to correct area for map var_map_cur
+  lda #$00 ;offset counter to 00
+  ldx var_map_cur
   inx
-  lda #$00                      ; start at position zero
-position_loop_add_loop
-  adc #$07                      ; add +7 byte size to offset position in A
-  dex                           ; loop counter--
-  bne position_loop_add_loop    ; if not zero keep looping
+position_loop_add_map_loop
+  dex
+  beq position_loop_add_map_loop_exit
+  clc
+  adc #$30 ;size of positions in a single map
+  jmp position_loop_add_map_loop
+position_loop_add_map_loop_exit
+  sta $16           ; store LOW byte of base positions for map infos ($60XX) in $16 / $17, for further look up
+  ; z$16 now at position 0 for map var_map_cur
+calc_map_position
+  lda #$00          ; zero A for position offset, to be stored in Y and saved in $18
+  ldx var_map_pos
+position_loop_add_pos_loop
+  cpx #$00
+  beq position_loop_add_pos_loop_exit
+  clc
+  adc #$08 ;size of single position entry
+  dex
+  jmp position_loop_add_pos_loop
+position_loop_add_pos_loop_exit
   tay                           ; result of looped addition A -> Y, for pointer offset usage
-  sty $18                       ; temp store Y -> Zero page $18
+  sty $18                       ; temp store Y (position offset) -> Zero page $18
 set_sprite_position
-;  ldy $18                       ; load this position pointer offset
-;  lda ($16), Y                  ; load scr-x value of position, multiply by 8, store in sprite 0 X mem loc
-;  ; TODO : if Y*8 > 256 then need to set MSB sprite register at $D010
-;  lsr
-;  lsr
-;  lsr
-;  sta $D000
-;  iny                           ; next value
-;  lda ($16), Y                  ; load scr-y value of position, multiply by 8, store in sprite 0 Y mem loc
-;  lsr
-;  lsr
-;  lsr
-;  sta $D001
-  lda #$00
+  ldy $18                       ; load this position pointer offset
+  lda ($16), Y                  ; load scr-x value of position, multiply by 8, add 32 for offset, store in sprite 0 X mem loc
+  asl
+  asl
+  ;if Y*8 > 256 then need to set MSB sprite register at $D010
+  tax ;save A -> X
+  and #$80  ;mas A with 10000000
+  cmp #$80
+  beq set_sprite_position_x_second_pos_set   ; set MSB if next shift will require more than 8 bits, if not, clear MSB
+  lda #$00                      ; clear MSB register for sprite 0 only
+  sta $D010
+  txa ; restore X -> A
+  asl
+  jmp set_sprite_position_y
+set_sprite_position_x_second_pos_set
+  lda #$01                      ; set MSB register for sprite 0 only
+  sta $D010
+  txa ; restore X -> A
+  asl
+set_sprite_position_y
+  clc
+  adc #$08
   sta $D000
+  iny                           ; next value
+  lda ($16), Y                  ; load scr-y value of position, multiply by 8, add 48, add store in sprite 0 Y mem loc
+  asl
+  asl
+  asl
+  clc
+  adc #$10
   sta $D001
   lda $D015                     ; finally, turn sprite on, OR sprite enable reg with 0x01, bit 0 for sprite 0
   ora #$01
@@ -117,7 +143,46 @@ set_sprite_position
 ;  sta $05F4
 ;  lda #$02      ; red foreground
 ;  sta $D9F4
+  ; *** MAIN LOOP
+  iny
+  ; note that Y will be base+2 at this point
+  ; save Y value to z$19
+  sty $19
+map_screen_wait_for_input
+  jsr FN_GETIN
+  cmp #$00
+  beq map_screen_wait_for_input
+  cmp #$57        ; check if W, up
+  beq map_screen_input_up
+  cmp #$53        ; check if S, down
+  beq map_screen_input_down
+  cmp #$41        ; check if A, left
+  beq map_screen_input_left
+  cmp #$44        ; check if D, right
+  beq map_screen_input_right
+  cmp #$20        ; check if space, interact
+  beq map_screen_wait_for_input
+  jmp map_screen_wait_for_input
+map_screen_input_right ; Y to up data using passtrough technique
+  inc $19
+map_screen_input_left
+  inc $19
+map_screen_input_down
+  inc $19
+map_screen_input_up
+  inc $19         
+map_screen_input_read
+  ldy $19
+  lda ($16), Y        ; load new position
+  cmp #$FF            ; if FF then can't move
+  beq calc_map_position   ; recalc, even no change
+  sta var_map_pos
+  jmp calc_map_position
+map_screen_input_action
+  ; TODO : interaction
+  jmp map_screen_wait_for_input
 
+infinite_loop_unused
   jmp *
 
 ;==========================================================
@@ -220,10 +285,6 @@ draw_screen_skip_1
 const_bag_size
   !byte $1E     ; size of bag (30)
 ; addresses
-const_map_meta_base_low
-  !byte $00 ; low byte of addr $6000
-const_map_meta_base_high
-  !byte $60 ; high byte of addr $6000
 const_map_img_base_low
   !byte $70
 
@@ -247,20 +308,14 @@ var_map_pos
 ;==========================================================
 * = $6000
 
-map_meta_data_table
-  !byte $08,$60
-  !byte $08,$60 ; TODO : change to position data
-  !byte $08,$60 ; TODO : change to position data
-  !byte $08,$60 ; TODO : change to position data
-
-map_meta_data_1
-;0-bedroom, 1 + (6 * 7) = 43 bytes of info
-  !byte $05,$11,$00,$FF,$01,$FF,$FF   ;pos(05/0x05,17/0x11); pos0 - type item list, #0; U:-1, D: 1, L:-1, R:-1
-  !byte $05,$17,$01,$00,$FF,$FF,$02   ;pos(05/0x05,23/0x17); pos1 - type item list, #1; U: 0, D:-1, L:-1, R: 2
-  !byte $0E,$15,$02,$FF,$FF,$01,$03   ;pos(14/0x0E,21/0x15); pos2 - type item list, #2; U:-1, D:-1, L: 1, R: 3
-  !byte $1B,$17,$81,$05,$FF,$02,$04   ;pos(27/0x1B,23/0x17); pos3 - type link to,   #1; U: 5, D:-1, L: 2, R: 4
-  !byte $22,$16,$83,$05,$03,$03,$FF   ;pos(34/0x22,22/0x16); pos4 - type link to  , #3; U: 5, D: 3, L: 3, R:-1
-  !byte $1A,$0D,$03,$FF,$03,$FF,$04   ;pos(26/0x1A,15/0x0D); pos5 - type item list, #3; U:-1, D: 3, L:-1, R: 4
+map_meta_data
+;0-bedroom, 1 + (6 * 8) = 48 bytes (0x30) of info
+  !byte $05,$11,$00,$FF,$01,$FF,$FF,$00   ;pos(05/0x05,17/0x11); pos0 - type item list, #0; U:-1, D: 1, L:-1, R:-1; empty
+  !byte $05,$17,$01,$00,$FF,$FF,$02,$00   ;pos(05/0x05,23/0x17); pos1 - type item list, #1; U: 0, D:-1, L:-1, R: 2; empty
+  !byte $0E,$15,$02,$FF,$FF,$01,$03,$00   ;pos(14/0x0E,21/0x15); pos2 - type item list, #2; U:-1, D:-1, L: 1, R: 3; empty
+  !byte $1B,$17,$81,$05,$FF,$02,$04,$00   ;pos(27/0x1B,23/0x17); pos3 - type link to,   #1; U: 5, D:-1, L: 2, R: 4; empty
+  !byte $22,$16,$83,$05,$03,$03,$FF,$00   ;pos(34/0x22,22/0x16); pos4 - type link to  , #3; U: 5, D: 3, L: 3, R:-1; empty
+  !byte $1A,$0D,$03,$FF,$03,$FF,$04,$00   ;pos(26/0x1A,15/0x0D); pos5 - type item list, #3; U:-1, D: 3, L:-1, R: 4; empty
 ;1-kitchen1
 
 ;==========================================================
